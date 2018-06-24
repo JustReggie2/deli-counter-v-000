01e68a9e7fdc4e2450386dd02dbd6798bcd8f7b7
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-06-24 16:14:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require 'pry'
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length >= 1
+    the_line = "The line is currently:"
+    katz_deli.each_with_index do |customer, index|
+      the_line << " #{index+1}. #{customer}"
+    end
+    puts the_line
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, customer_name)
+end
