d352fc46e24f3c52c53fcee49c6d74e54e708701
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-06-24 16:47:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,8 @@ def take_a_number(katz_deli, customer_name)
   if katz_deli == []
     katz_deli << customer_name
     puts "Welcome, #{customer_name}. You are number 1 in line."
-  else katz_deli << customer_name
+  else katz_deli.length >= 1
+    katz_deli << customer_name
     katz_deli.each_with_index do |customer, index|
       puts "Welcome, #{customer}. You are number #{index+1} in line."
     end
